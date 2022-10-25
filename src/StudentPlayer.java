f80fdd0ca04bb069847004d8b459392261470895
KisRusi <kisrusi01@gmail.com>
2022-10-25 13:39:16 +0200

    ..

--- src/StudentPlayer.java
+++ src/StudentPlayer.java
@@ -11,14 +11,14 @@ public class StudentPlayer extends Player{
     public int step(Board board)
     {
 
-        return pickMove(board,this.playerIndex);
+        return minimax(board,3,-1000000000, 100000000,2);
     }
 
     public int minimax(Board board, int depth,int alpha, int beta, int player)
     {
         if(depth == 0 || board.gameEnded())
         {
-            return heuristic(board.getState());
+            return evalScore(board,2);
         }
         ArrayList<Integer> validsteps;
         validsteps = board.getValidSteps();
@@ -27,6 +27,9 @@ public class StudentPlayer extends Player{
             int maxEval = -100000000;
             for(Integer i : validsteps)
             {
+                int row = nextRow(board,i);
+                Board temp =new  Board(board);
+                tryMove(temp,row,i,2);
                 int eval = minimax(board, depth-1, alpha, beta,1);
                 maxEval = Math.max(maxEval,eval);
                 alpha = Math.max(alpha,eval);
@@ -41,6 +44,9 @@ public class StudentPlayer extends Player{
             int minEval = 100000000;
             for(int i : validsteps)
             {
+                int row = nextRow(board, i);
+                Board temp = new Board(board);
+                tryMove(temp, row, i,1);
                 int eval = minimax(board,depth - 1, alpha, beta, 2);
                 minEval = Math.min(minEval,eval);
                 beta = Math.min(beta, eval);
@@ -51,40 +57,35 @@ public class StudentPlayer extends Player{
         }
     }
 
-
-    int heuristic(int[][] s) {
-        int result = 0;
-        int i, j;
-
-        //check horizontals
-        for(i=0; i<6; i++)
-            for(j=0; j<=7-4; j++){
-                if(s[i][j]!= 2 && s[i][j+1]!= 2 && s[i][j+2]!= 2 && s[i][j+3]!= 2)
-                    result++;
-                if(s[i][j]!= 1 && s[i][j+1]!= 1 && s[i][j+2]!= 1 && s[i][j+3]!= 1)
-                    result--;
-            }
-
-        //check verticals
-        for(i=0; i<=6-4; i++)
-            for(j=0; j<7; j++){
-                if(s[i][j]!= 2 && s[i+1][j]!= 2 && s[i+2][j]!= 2 && s[i+3][j]!= 2 )
-                    result++;
-                if(s[i][j]!= 1 && s[i+1][j]!= 1 && s[i+2][j]!= 1 && s[i+3][j]!= 1 )
-                    result--;
-            }
-
-        return result;
+    int evalWindow(int[] window,int playerState)
+    {
+        int score = 0;
+        if(Arrays.stream(window).filter(c->c ==2).count()==4)
+            score += 100;
+        else if(Arrays.stream(window).filter(c-> c ==2).count()==3 && Arrays.stream(window).filter(c -> c==0).count()==1)
+            score += 5;
+        else if(Arrays.stream(window).filter(c-> c ==2).count()==2 && Arrays.stream(window).filter(c -> c==0).count()==2)
+            score +=2;
+        if(Arrays.stream(window).filter(c-> c ==1).count()==3 && Arrays.stream(window).filter(c -> c==0).count()==1)
+            score -= 80;
+        return score;
     }
-
-
-
     int evalScore(Board board, int playerState)
     {
         int[][] boardstate = board.getState();
         int[] row = new int[7];
+        int[] col = new int[6];
+        int[] center = new int[6];
         int[] window= new int[4];
+        double floordiv = 7/2;
         int score = 0;
+
+        for (int i = 0; i<6; i++)
+        {
+            center[i] = boardstate[i][Math.floorDiv(7,2)];
+            score += (Arrays.stream(center).filter(c -> c == 2).count()) * 3;
+        }
+
 
        for(int k = 0; k <6; k++)
        {
@@ -98,14 +99,49 @@ public class StudentPlayer extends Player{
                {
                    window[a] = row[b+a];
                }
-               if(Arrays.stream(window).filter(c-> c==1 || c ==2).count()==4)
-                   score += 100;
-               else if (Arrays.stream(window).filter(c-> c==1 || c ==2).count()==3 && Arrays.stream(window).filter(c-> c==0).count()==1)
-                   score+= 10;
+               score += evalWindow(window,playerState);
 
            }
        }
+       for(int k = 0; k < 7; k++)
+       {
+            for(int i = 0; i<6; i++)
+            {
+                col[i] = boardstate[i][k];
+            }
+            for(int b = 0; b<6-3; b++)
+            {
+                for(int a = 0; a<4; a++)
+                {
+                    window[a] = col[a+b];
+                }
+                score += evalWindow(window,playerState);
+            }
+       }
 
+        for(int k = 0; k<7-3; k++)
+        {
+            for(int i = 0; i<6-3; i++)
+            {
+                for(int a = 0; a<4; a++)
+                {
+                    window[a] = boardstate[i+a][k+a];
+                }
+                score += evalWindow(window,playerState);
+            }
+        }
+
+        for(int k = 0; k<7-3; k++)
+        {
+            for(int i = 0; i<6-3; i++)
+            {
+                for(int a = 0; a<4; a++)
+                {
+                    window[a] = boardstate[i+a][k+3-a];
+                }
+                score += evalWindow(window,playerState);
+            }
+        }
 
 
         return score;
@@ -120,7 +156,7 @@ public class StudentPlayer extends Player{
         int bestCol = validsteps.get(rand.nextInt(validsteps.size()));
         for(int col = 0; col<validsteps.size();col++)
         {
-            int row = nextOpenRow(board,col);
+            int row = nextRow(board,col);
             Board temp = new Board(board);
             tryMove(temp,row,col,playerState);
             score =evalScore(temp,playerState);
@@ -133,14 +169,14 @@ public class StudentPlayer extends Player{
         return bestCol;
     }
 
-    int nextOpenRow(Board board, int col)
+    int nextRow(Board board, int col)
     {
         for(int i = 5; i>-1;i--)
         {
             if(board.getState()[i][col]==0)
                 return i;
         }
-            throw new RuntimeException();
+            return 0;
     }
     void tryMove(Board board, int row, int col, int playerState)
     {
